dfc220c393660e4475e892cc0692974f6e824b8b
Manoel Rocha <114105012+ManoelRochaS@users.noreply.github.com>
2023-01-04 19:28:50 -0300

    Add files via upload

--- javascript/script.js
+++ javascript/script.js
@@ -1,65 +1,48 @@
 
-/*Function that coordinates the Digital Mode Display*/
+/*Declaração das Variáveis necessárias*/
+const buttonDigital = document.getElementById("buttonDigital");
+const buttonAnalogic = document.getElementById("buttonAnalogic");
+const boxDigital = document.getElementById("boxDigital");
+const boxAnalogic = document.getElementById("boxAnalogic");
+const displayText = document.getElementById("displayText");
+const Time = document.getElementsByClassName("Time");
 let controlDigital = true;
-function editDisplayDigital(){
-    if(controlDigital){
-        document.body.style.background="linear-gradient( 160deg,#0A0A0B, #215BF0)";
-        document.getElementById("InterfaceDigital").style.display="flex";
-        controlDigital = false;
-    }
-    else{
-        document.body.style.background="linear-gradient( 70deg,#0A0A0B, #215BF0)";
-        document.getElementById("InterfaceDigital").style.display="none";
-        controlDigital = true;
-    }
-}
+let controlAnalogic = true;
 
-/*Function that coordinates the Analog Mode Display*/
-let controlAnalog = true;
-function editDisplayAnalog(){
-    if(controlAnalog){
-        document.body.style.background="linear-gradient( 160deg,#0A0A0B, #215BF0)";
-        document.getElementById("InterfaceAnalog").style.display="flex";
-        controlAnalog = false;
-    }
-    else{
-        document.body.style.background="linear-gradient( 70deg,#0A0A0B, #215BF0)";
-        document.getElementById("InterfaceAnalog").style.display="none";
-        controlAnalog = true;
-    }
-}
+/*Configuração dos Estilos e Css da Aplicação*/
+buttonDigital.addEventListener("click", ()=>{
+    boxDigital.style.display = (controlDigital) ? "flex" : "none";
+    controlDigital = (controlDigital) ? false : true; 
+})
 
-/*Function that coordinates the all Display and and respective individualities*/
-const ActionDate = ()=>{
-    /*Declaration of Control Variables of this function*/
-    const Doc = document.getElementById("DisplayDigital");
+buttonAnalogic.addEventListener("click", ()=>{
+    boxAnalogic.style.display = (controlAnalogic) ? "flex" : "none";
+    controlAnalogic = (controlAnalogic) ? false : true; 
+})
+
+/*Configuração e Construção dos Displays (Digital e Analógico)*/
+setInterval(()=>{
     const CurrentDate = new Date();
-    const Hour = CurrentDate.getHours();
-    const Minute = CurrentDate.getMinutes();
-    const Second = CurrentDate.getSeconds();
+    //Configuração Display Digital
+    displayText.innerHTML = toString(edtTime(CurrentDate.getHours()), edtTime(CurrentDate.getMinutes()), edtTime(CurrentDate.getSeconds()));
+    //Configuração Display Analógico
+    innerAnalog(CurrentDate.getHours(), CurrentDate.getMinutes(), CurrentDate.getSeconds(), 0);
+},1000);
 
-    /*Analysis of the searched data and formatting them when necessary for the Digital Display*/
-    const NewHour = Hour < 10 ? `0${Hour}` : Hour;
-    const NewMinute = Minute < 10 ? `0${Minute}` : Minute;
-    const NewSecond = Second < 10 ? `0${Second}` : Second;
-
-    /*Time analysis and time and period printing*/
-    if(Hour <12){
-        Doc.innerHTML = `${NewHour} : ${NewMinute} : ${NewSecond} AM`;
-    }
-    else{
-        Doc.innerHTML = `${NewHour} : ${NewMinute} : ${NewSecond} PM`;
-    }
-    
-    /*Calculation for setting the Analog Clock*/
-    HourRotation = 30 * Hour + Minute / 2;
-    MinuteRotation = 6 * Minute;
-    SecondRotation = 6 * Second;
-
-    /*Analog Clock Printing*/
-    document.getElementById("Hours").style.transform=`rotate(${HourRotation}deg)`;
-    document.getElementById("Minutes").style.transform=`rotate(${MinuteRotation}deg)`;
-    document.getElementById("Seconds").style.transform=`rotate(${SecondRotation}deg)`;
+/* --- Funções Auxiliares - Display Digital */
+function toString(Hour, Minute, Second){
+    return (Hour < 12) ? `${Hour} : ${Minute} : ${Second} AM` : `${Hour} : ${Minute} : ${Second} PM`;
+}
+function edtTime(Time){
+    return (Time < 10) ? `0${Time}` : Time;
 }
 
-setInterval(()=>{ActionDate()}, 1000)
+/* --- Funções Auxiliares - Display Analógico*/
+function innerAnalog(Hour, Minute, Second, control){
+    for(var i = 0; i < 3; i++){
+        if(i==0){control = (30 * Hour + Minute / 2)};
+        if(i==1){control = (6 * Minute)};
+        if(i==2){control = (6 * Second)};
+        Time[i].style.transform = `rotate(${control}deg)`;
+    }
+}
