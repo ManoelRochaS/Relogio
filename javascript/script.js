9ffe1ac5e25e1397a5b8c81f3e763bf764e0825b
Manoel Rocha <114105012+ManoelRochaS@users.noreply.github.com>
2023-01-05 13:22:10 -0300

    Add files via upload

--- javascript/script.js
+++ javascript/script.js
@@ -1,15 +1,17 @@
 
-/*Declaração das Variáveis necessárias*/
+/*Declaração de Constantes (Id e Class) - buttonDigital, buttonAnalogic, boxDigital, boxAnalogic, displayText e Time*/
 const buttonDigital = document.getElementById("buttonDigital");
 const buttonAnalogic = document.getElementById("buttonAnalogic");
 const boxDigital = document.getElementById("boxDigital");
 const boxAnalogic = document.getElementById("boxAnalogic");
 const displayText = document.getElementById("displayText");
 const Time = document.getElementsByClassName("Time");
+
+/*Declaração de Variáveis (Controladores) - controlDigital e controlAnalogic*/
 let controlDigital = true;
 let controlAnalogic = true;
 
-/*Configuração dos Estilos e Css da Aplicação*/
+/*Funções Principais (CSS) - () /Digital/, () /Analógico/ e setInterval()*/
 buttonDigital.addEventListener("click", ()=>{
     boxDigital.style.display = (controlDigital) ? "flex" : "none";
     controlDigital = (controlDigital) ? false : true; 
@@ -20,16 +22,17 @@ buttonAnalogic.addEventListener("click", ()=>{
     controlAnalogic = (controlAnalogic) ? false : true; 
 })
 
-/*Configuração e Construção dos Displays (Digital e Analógico)*/
 setInterval(()=>{
     const CurrentDate = new Date();
+
     //Configuração Display Digital
     displayText.innerHTML = toString(edtTime(CurrentDate.getHours()), edtTime(CurrentDate.getMinutes()), edtTime(CurrentDate.getSeconds()));
+    
     //Configuração Display Analógico
-    innerAnalog(CurrentDate.getHours(), CurrentDate.getMinutes(), CurrentDate.getSeconds(), 0);
+    innerAnalog(CurrentDate.getHours(), CurrentDate.getMinutes(), CurrentDate.getSeconds());
 },1000);
 
-/* --- Funções Auxiliares - Display Digital */
+/*Funções Auxiliares (CSS - DisplayDigital) - toString(Hour, Minute, Second) e edtTime(Time)*/
 function toString(Hour, Minute, Second){
     return (Hour < 12) ? `${Hour} : ${Minute} : ${Second} AM` : `${Hour} : ${Minute} : ${Second} PM`;
 }
@@ -37,8 +40,9 @@ function edtTime(Time){
     return (Time < 10) ? `0${Time}` : Time;
 }
 
-/* --- Funções Auxiliares - Display Analógico*/
-function innerAnalog(Hour, Minute, Second, control){
+/*Funções Auxiliares ( - DisplayAnalogic) - innerAnalog(Hour, Minute, Second)*/
+function innerAnalog(Hour, Minute, Second){
+    var control;
     for(var i = 0; i < 3; i++){
         if(i==0){control = (30 * Hour + Minute / 2)};
         if(i==1){control = (6 * Minute)};
